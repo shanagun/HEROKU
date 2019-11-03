b9886b9ffccbcd0c2dfbad1f34e302e28f146e6b
Pak <andrei-pak@outlook.fr>
2019-11-03 22:42:29 +0100

    add app

---
+++ app/models/gossip.rb
@@ -0,0 +1,11 @@
+class Gossip < ApplicationRecord
+  belongs_to :user
+  has_many :gossip_tags
+  has_many :tags, through: :gossip_tags
+  has_many :comments
+  has_many :likes
+
+    #validation des attributs
+    validates :title, length: {in: 3..14}
+    validates :content, presence: true
+end
